ede5d3a91d3467eedae0f3c3c0d6f84fc4dfb117
Marcela de Paula <91508817+mamadipaula@users.noreply.github.com>
2022-06-04 13:55:44 -0300

    Update conexao.php

--- dao/conexao/conexao.php
+++ dao/conexao/conexao.php
@@ -1,7 +1,7 @@
 <?php
     class Conexao{
         function getConexao(){
-            $banco = new mysqli("localhost","root","879262","projetophpf");
+            $banco = new mysqli("localhost","root","","");
             return $banco;
             if ($banco -> conect_errno) {
                 echo "Erro" + $banco -> error;
